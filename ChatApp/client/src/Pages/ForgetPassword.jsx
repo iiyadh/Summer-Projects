bf1464229c0886ad68f849f7b799132befb64901
Tabai Iyadh <tabaiiyadh317@gmail.com>
2025-06-22 17:50:41 +0100

    Add Loader Effect to ForgetPassword page

--- ChatApp/client/src/Pages/ForgetPassword.jsx
+++ ChatApp/client/src/Pages/ForgetPassword.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom';
 import '../styles/Auth.css';
 import { useRef } from 'react';
+import { useLoadingStore } from '../store/loadingStore';
+import  Loader  from '../effects/Loader';
 
 
 const ForgetPassword = () => {
+    const { loading, show, hide } = useLoadingStore();
     const fromRef = useRef();
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
+        show();
+        setTimeout(() => {
+            hide();
+        }, 2000);
         const formData = new FormData(fromRef.current);
         const email = formData.get('email');
         console.log("Reset link sent to:", email);
@@ -23,9 +30,9 @@ const ForgetPassword = () => {
                 <form className="auth-form" ref={fromRef} onSubmit={(e)=>handleSubmit(e)}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input id="email" type="email" name='email' placeholder="Enter your email" required />
+                        <input id="email" type="email" name='email' placeholder="Enter your email" disabled={loading} required />
                     </div>
-                    <button type="submit" className="auth-button">Send Reset Link</button>
+                    <button type="submit" className="auth-button" disabled={loading}>{loading ? <Loader /> :"Send Reset Link"}</button>
                 </form>
                 <div className="auth-link">
                     <p>Remember your password? <Link to="/login">Back to Login</Link></p>
